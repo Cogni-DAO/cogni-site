b75253f8a52d22676c53198685680d43174e8f01
derekg1729 <derekgranito@gmail.com>
2025-06-16 04:10:58 -0700

    feat: add namespace support to data hooks - Add useNamespace hook to read namespace from URL params - Update useBlocks, useBlock, useBlocksByIds to accept namespace parameter - Maintain backward compatibility with existing hook usage

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export { useBlocks, useBlocksByIds } from './useBlocks';
 export { useBlock } from './useBlock';
+export { useNamespace } from './useNamespace';
 export { useWorkItemBlocks } from './useWorkItemBlocks';
 export { useFilteredBlocks } from './useFilteredBlocks';
 export { useToast } from './use-toast';
